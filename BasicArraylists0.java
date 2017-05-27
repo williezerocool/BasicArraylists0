2b05f252add898dddfe817ee87fb896182d5a18f
Willie Suggs <klbooker09@gmail.com>
2017-05-27 14:36:34 -0500

    Add files via upload

---
+++ BasicArraylists0.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package basicarraylists0;
+
+import java.util.ArrayList;
+/**
+ *
+ * @author kendrabooker
+ */
+public class BasicArraylists0 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     
+        ArrayList<Integer> arrList = new ArrayList<>();
+        
+        arrList.add(-113);
+        arrList.add(-113);
+        arrList.add(-113);
+        arrList.add(-113);
+        arrList.add(-113);
+        arrList.add(-113);
+        arrList.add(-113);
+        arrList.add(-113);
+        arrList.add(-113);
+        arrList.add(-113);
+        
+       
+        
+            System.out.println(arrList);
+        
+        
+    }
+    
+}
